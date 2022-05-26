1d920a165a94f8210d54f1e2fa14b60631cb4f0e
CDP <kevinchen345@gmail.com>
2022-05-26 20:10:51 +1000

    made fitcoin inherit erc20

--- fitcoin_token.sol
+++ fitcoin_token.sol
@@ -1,8 +1,16 @@
 pragma solidity ^0.5.0;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/math/SafeMath.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Detailed.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Mintable.sol";
 
-contract Fitcoin_Token {
+contract Fitcoin_Token is ERC20, ERC20Detailed, ERC20Mintable{
+
+    constructor (string memory name, string memory symbol, uint initial_supply)ERC20Detailed(name, symbol, 0) public{
+
+    }
+    /*
     using SafeMath for uint;
 
     address payable owner = msg.sender;
@@ -30,4 +38,5 @@ contract Fitcoin_Token {
         require(msg.sender == owner, "You do not have permission to mint tokens!");
         balances[recipient] = balances[recipient].add(value);
     }
+    */
 }
