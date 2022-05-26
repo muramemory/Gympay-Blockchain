03f35a224cb375b2ce2bf46540ad60b975079ac4
CDP <kevinchen345@gmail.com>
2022-05-26 22:58:54 +1000

    added in some functions that need to be implemented and set values in constructor

--- fitcoin_token.sol
+++ fitcoin_token.sol
@@ -1,42 +1,43 @@
 pragma solidity ^0.5.0;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/math/SafeMath.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Detailed.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Mintable.sol";
 
+
+/*
+Coin Description
+*/
 contract Fitcoin_Token is ERC20, ERC20Detailed, ERC20Mintable{
 
-    constructor (string memory name, string memory symbol, uint initial_supply)ERC20Detailed(name, symbol, 0) public{
-
-    }
     /*
-    using SafeMath for uint;
-
-    address payable owner = msg.sender;
-    string public symbol = "FIT";
-    uint public exchange_rate = 100;
-
-    mapping(address => uint) balances;
-
-    function balance() public view returns(uint) {
-        return balances[msg.sender];
-    }
-
-    function transfer(address recipient, uint value) public {
-        balances[msg.sender] = balances[msg.sender].sub(value);
-        balances[recipient] = balances[recipient].add(value);
-    }
-
-    function purchase() public payable {
-        uint amount = msg.value.mul(exchange_rate);
-        balances[msg.sender] = balances[msg.sender].add(amount);
-        owner.transfer(msg.value);
-    }
-
-    function mint(address recipient, uint value) public {
-        require(msg.sender == owner, "You do not have permission to mint tokens!");
-        balances[recipient] = balances[recipient].add(value);
-    }
+        Initialise coin as Fitcoin (FIT) with an initial supply of 0 (by not declaring it)
+        and 0 decimals so we can just look at whole numbers
     */
+
+    constructor () ERC20Detailed("Fitcoin", "Fit", 0) public{
+        
+    }
+
+    /*
+        Purchase Function
+
+        @TODO: Call inherited mint function
+    */
+    function deposit (address account, uint256 amount) public{
+
+    }
+
+        /*
+        Purchase Function
+
+        @TODO: Call inherited burn function
+    */
+    function withdraw (address account, uint256 amount) public{
+
+    }
+
 }
+
+
+
