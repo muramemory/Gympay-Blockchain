18d418d84f92584f6de9745ac9e04229a09a2e6f
Anthony <anthonymura6@gmail.com>
2022-06-02 19:12:22 +1000

    added streamlit sidebar menu

--- app.py
+++ app.py
@@ -6,16 +6,39 @@ from dotenv import load_dotenv
 import streamlit as st
 from PIL import Image
 import pandas as pd
+from streamlit_option_menu import option_menu
 
 load_dotenv()
 
+# Setting for fullpage
 
+st.set_page_config(page_icon=(":shark:"))
+
+### Sidebar 
 ### Create Top Bar ###
 
 st.markdown(
     "##### Made in [![image_add](https://media-thumbs.golden.com/Ce1V_LeVgumkUOeYYkixMAoRQNU=/200x200/smart/golden-storage-production.s3.amazonaws.com%2Ftopic_images%2Fe509a9c1bb3541c38c30b22a2173d456.png)](https://streamlit.io/) by [Gympay Team](https://github.com/muramemory/Gympay-Blockchain)"
 )
 
+# Sidebar Menu
+
+
+with st.sidebar:
+    selected = option_menu(
+        menu_title="Main Menu", # required
+        options=["Home", "Projects", "Contact"], #required
+        icons=["house","book","envelope"],
+        menu_icon="cast",
+        default_index=0,
+    )
+
+if selected == "Home":
+    st.title(f"You have selected {selected}")
+if selected == "Projects":
+    st.title(f"You have selected {selected}")
+if selected == "Contact":
+    st.title(f"You have selected {selected}")
 
 
 # Define and connect a new Web3 provider
@@ -57,11 +80,14 @@ contract = load_contract()
 image = Image.open('Images/Gympay.png')
 st.image(image)
 
-st.text('Gympay is a service that enables you to pay for your gym membership with')
-st.text('cryptocurrency. Blockchain technology enables cheaper and faster transactions') 
-st.text('which gives gym owners opportunities to offer great services at great prices.')
-st.text('Purchase Fitcoin below and use your digital wallet to scan into partnered gym')
-st.text('facilities for instant access and opportunties to earn rewards.')
+st.markdown("""
+Gympay  is a service that enables you to pay for your gym membership with cryptocurrency. Blockchain technology enables cheaper and faster 
+transactions which gives gym owners opportunities to offer great services at great prices.
+Purchase Fitcoin below and use your digital wallet to scan into partnered gym
+facilities for instant access and opportunties to earn rewards.
+
+""")
+
 
 ################################################################################
 # Select wallet
